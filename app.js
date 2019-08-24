b0956e9e7515623e3a2f801108b2cb38a68a4a3a
kenkarolz99 <kenkarolz99@gmail.com>
2019-08-24 21:36:54 +0300

    Added url

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(session({
     secret: 'karokojnr',
     cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
     store: new MongoStore({
-      url: 'mongodb://karokojnr:karokojnr@cluster0-shard-00-00-ubthk.gcp.mongodb.net:27017,cluster0-shard-00-01-ubthk.gcp.mongodb.net:27017,cluster0-shard-00-02-ubthk.gcp.mongodb.net:27017/cms',
+      url: 'mongodb://karokojnr:<password>@cluster0-shard-00-00-ubthk.gcp.mongodb.net:27017,cluster0-shard-00-01-ubthk.gcp.mongodb.net:27017,cluster0-shard-00-02-ubthk.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
       autoReconnect: true,
     })  
 }));
--- config/prod-database.js
+++ config/prod-database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongodbUrl: 'mongodb://karokojnr:karokojnr@cluster0-shard-00-00-ubthk.gcp.mongodb.net:27017,cluster0-shard-00-01-ubthk.gcp.mongodb.net:27017,cluster0-shard-00-02-ubthk.gcp.mongodb.net:27017/cms'
+    mongodbUrl: 'mongodb://karokojnr:<password>@cluster0-shard-00-00-ubthk.gcp.mongodb.net:27017,cluster0-shard-00-01-ubthk.gcp.mongodb.net:27017,cluster0-shard-00-02-ubthk.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 }
