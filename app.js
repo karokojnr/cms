b12cbdbfb91f041ad95f0b8e0c9bf9029444cb3f
kenkarolz99 <kenkarolz99@gmail.com>
2019-08-24 20:53:32 +0300

    debugging connect session

--- app.js
+++ app.js
@@ -31,9 +31,14 @@ mongoose.connect( mongodbUrl, { useNewUrlParser: true })
 app.use(upload());
 
 app.use(session({
-    secret: "karokojnr",
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
+    store: new MongoStore({
+      url: process.env.MONGODB_DEV,
+      autoReconnect: true,
+    })  
 }));
 app.use(flash());
 
