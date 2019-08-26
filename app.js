49bb52c95238de45d29857b8af74f8d16653f204
kenkarolz99 <kenkarolz99@gmail.com>
2019-08-26 15:48:26 +0300

    Added multer

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const passport = require('passport');
 const { mongodbUrl } = require('./config/database');
+const multer = require('multer');
+const upload = multer({ dest: path.join(__dirname, 'public/uploads')});
 const app = express();
 
 
