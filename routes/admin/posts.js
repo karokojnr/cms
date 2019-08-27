e0f24b24890c07ba9f0194464c885e64a9b2b677
kenkarolz99 <kenkarolz99@gmail.com>
2019-08-27 17:13:53 +0300

    Fixing cloudinary 4

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -57,7 +57,7 @@ router.post('/create',(req,res) => {
     let file = req.files.file;
     filename = Date.now() + '-' + file.name;
 
-    cloudinary.uploader.upload('/public/uploads',(err, result)=>{
+    cloudinary.uploader.upload('file',(err, result)=>{
         if(err) return err;
     });
     // file.mv('./public/uploads/' + filename,(err) => {
