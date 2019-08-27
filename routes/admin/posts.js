b5220fab9cb21b4766ac907674dee9281e020a58
kenkarolz99 <kenkarolz99@gmail.com>
2019-08-27 13:53:09 +0300

    Cloudinary fixed 5

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -62,19 +62,19 @@ router.post('/create', (req,res) => {
 
     let file = req.files.file;
     filename = Date.now() + '-' + file.name;
-    cloudinary.uploader.upload(file.tempFilePath, (err,result)=>{
-        res.send({
-            success: true,
-            result
-        });
-    });
+    // cloudinary.uploader.upload(file.tempFilePath, (err,result)=>{
+    //     res.send({
+    //         success: true,
+    //         result
+    //     });
+    // });
 }
 
     //error -> ./public/uploads
-    // file.mv('./public/uploads/' + filename,(err) => {
-    //     if (err) throw err;
-    // });
-    // }
+    file.mv('./public/uploads/' + filename,(err) => {
+        if (err) throw err;
+    });
+    }
     let allowComments =true;
     if (req.body.allowComments){
         allowComments = true;
